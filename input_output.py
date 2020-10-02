59cc1c258ed9569fb497d898088be8d2874de4b5
Song Yang <songyang1218@gmail.com>
2020-10-02 14:00:56 -0400

    Integrate timing into python

--- input_output.py
+++ input_output.py
@@ -5,8 +5,44 @@ import argparse
 from argparse import Namespace
 from sys import stdin
 from typing import Dict, Iterator
+import time
 
 parsed_args: Namespace
+
+class Timer:
+    __instance = None
+    is_timing = False
+
+    @staticmethod
+    def get_current_timer():
+        """
+        Get a singleton instance of the timer
+        """
+        if Timer.__instance is None:
+            raise Exception("No timer")
+        return Timer.__instance
+
+    def __init__(self, path: str):
+        """Creates a timer instance
+
+        Args:
+            path (str): Path to the file where to put the time
+        """
+        self.start = time.time()
+        self.path_to_log = path
+
+        # clean up the log file
+        open(self.path_to_log, 'w').close()
+
+        Timer.is_timing = True
+        Timer.__instance = self
+
+    def log(self):
+        """
+        Logs the elapsed time since instantiation
+        """
+        with open(self.path_to_log, 'a') as time_log:
+            time_log.write("{}\n".format(time.time() - self.start))
 
 
 def get_args() -> Dict[str, str]:
@@ -27,26 +63,34 @@ def get_args() -> Dict[str, str]:
     parser.add_argument('--output', dest='output_file',
                         help="Output file of the suggested corrections. By default, it is stdin.")
 
+    parser.add_argument('--time', dest='is_timing', action='store_const', const=True,
+                        default=False,
+                        help="Whether to time the execution of the script into a file called time_name-of-distance.csv")
+
     # choose one distance model
     distance_choices = parser.add_mutually_exclusive_group()
 
-    distance_choices.add_argument('--levenshtein', dest='distance', action='store_const',\
-        const='levenshtein',\
-            help="Uses the Levenshtein distance implementation from pyspellchecker")
+    distance_choices.add_argument('--levenshtein', dest='distance', action='store_const',
+                                  const='levenshtein',
+                                  help="Uses the Levenshtein distance implementation from pyspellchecker")
 
-    distance_choices.add_argument('--levenshtein2', dest='distance', action='store_const',\
-        const='levenshtein2',\
-            help="Uses the Levenshtein distance implementation from Levenshtein")
+    distance_choices.add_argument('--levenshtein2', dest='distance', action='store_const',
+                                  const='levenshtein2',
+                                  help="Uses the Levenshtein distance implementation from Levenshtein")
 
-    distance_choices.add_argument('--hamming', dest='distance', action='store_const',\
-        const='hamming',\
-            help="Uses the Hamming distance")
+    distance_choices.add_argument('--hamming', dest='distance', action='store_const',
+                                  const='hamming',
+                                  help="Uses the Hamming distance")
 
-    distance_choices.add_argument('--jarowinkler', dest='distance', action='store_const',\
-        const='jarowinkler',\
-            help="Uses the Jaro-Winkler distance")
+    distance_choices.add_argument('--jarowinkler', dest='distance', action='store_const',
+                                  const='jarowinkler',
+                                  help="Uses the Jaro-Winkler distance")
 
+    # parse the args
     args = parser.parse_args()
+
+    arg_dict = {"vocabulary": args.vocabulary, "input_file": args.input_file,
+                "distance": args.distance if args.distance is not None else "levenshtein"}
 
     # output file is emptied
     if args.output_file is not None:
@@ -54,8 +98,11 @@ def get_args() -> Dict[str, str]:
     global parsed_args
     parsed_args = args
 
-    return {"vocabulary": args.vocabulary, "input_file": args.input_file,\
-        "distance": args.distance if args.distance is not None else "levenshtein"}
+    # start timer if needed
+    if args.is_timing:
+        _ = Timer("time_{}.csv".format(arg_dict['distance']))
+
+    return arg_dict
 
 
 def get_misspelling() -> Iterator[str]:
@@ -77,6 +124,7 @@ def get_misspelling() -> Iterator[str]:
 
 def output(message: str):
     """Prints to the stdout or appends to the end of the given file.
+    Logs the time, if enabled in the command line.
 
     Args:
         message (str): String to output
@@ -86,3 +134,8 @@ def output(message: str):
     else:
         with open(parsed_args.output_file, 'a+') as out:
             out.write(message + "\n")
+    
+    # time
+    if Timer.is_timing:
+        timer = Timer.get_current_timer()
+        timer.log()
