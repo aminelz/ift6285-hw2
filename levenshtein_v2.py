2b195564b999be3737f9cd59f065377a92dc378d
Amine Laazizi <amine.laazizi@gmail.com>
2020-10-02 03:42:47 +0100

    Add alternative version of levenshtein distance Module

---
+++ levenshtein_v2.py
@@ -0,0 +1,38 @@
+from typing import Iterator
+from spellchecker import SpellChecker
+from input_output import output
+from Levenshtein import distance
+import numpy as np
+
+def levenshtein_distance2(words: Iterator[str], vocabulary: str):
+    """Corrects the words based on JaroWinkler distances
+
+    Args:
+        words (Iterator[str]): Iterator over the misspelled words
+        vocabulary (str) : dictionary holding words and their frequency
+    """
+    suggestions = []
+
+    for word in words:
+        distances = []
+        suggestions = []
+        vocab_list = list(vocabulary)
+        for (i,vocab) in enumerate(vocab_list):
+            distances.append(distance(word, vocab))
+        idx = np.array(distances).argsort()[:5]
+        
+    for i in range(5):
+        for j in range(i+1,5):
+            if distances[idx[i]] == distances[idx[j]]:
+                if vocabulary.get(vocab_list[idx[i]]) < vocabulary.get(vocab_list[idx[j]]):
+                    temp = idx[i] 
+                    idx[i] = idx[j]
+                    idx[j] = temp   
+
+    for i in idx:
+        suggestions.append()
+
+        output("{misspelled}\t{corrections}".format(
+            misspelled=word,
+            corrections="\t".join(suggestions)
+        ))  # may cause IO bottleneck
